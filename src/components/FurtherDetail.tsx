eb5fce08f04b738bc7e970e03a879e55609bf2c6
MulugetaNigus <mullerhihi@gmail.com>
2024-05-22 17:10:03 +0300

    adding more logic, when the data fetch from the api

--- src/components/FurtherDetail.tsx
+++ src/components/FurtherDetail.tsx
@@ -53,111 +53,212 @@ const FurtherDetail = () => {
                   <div className="col-md-6">
                     <h5>Ingredients</h5>
                     <ul>
-                      <li>
-                        {features.strIngredient1 !== "" &&
-                          features.strIngredient1}
-                      </li>
-                      <li>
-                        {features.strIngredient2 !== "" &&
-                          features.strIngredient2}
-                      </li>
-                      <li>
-                        {features.strIngredient3 !== "" &&
-                          features.strIngredient3}
-                      </li>
-                      <li>
-                        {features.strIngredient4 !== "" &&
-                          features.strIngredient4}
-                      </li>
-                      <li>
-                        {features.strIngredient5 !== "" &&
-                          features.strIngredient5}
-                      </li>
-                      <li>
-                        {features.strIngredient6 !== "" &&
-                          features.strIngredient6}
-                      </li>
-                      <li>
-                        {features.strIngredient7 !== "" &&
-                          features.strIngredient7}
-                      </li>
-                      <li>
-                        {features.strIngredient8 !== "" &&
-                          features.strIngredient8}
-                      </li>
-                      <li>
-                        {features.strIngredient9 !== "" &&
-                          features.strIngredient9}
-                      </li>
-                      <li>
-                        {features.strIngredient10 !== "" &&
-                          features.strIngredient10}
-                      </li>
-                      <li>
-                        {features.strIngredient11 !== "" &&
-                          features.strIngredient11}
-                      </li>
-                      <li>
-                        {features.strIngredient12 !== "" &&
-                          features.strIngredient12}
-                      </li>
-                      <li>
-                        {features.strIngredient13 !== "" &&
-                          features.strIngredient13}
-                      </li>
-                      <li>
-                        {features.strIngredient14 !== "" &&
-                          features.strIngredient14}
-                      </li>
-                      <li>
-                        {features.strIngredient15 !== "" &&
-                          features.strIngredient15}
-                      </li>
-                      <li>
-                        {features.strIngredient16 !== "" &&
-                          features.strIngredient16}
-                      </li>
-                      <li>
-                        {features.strIngredient17 !== "" &&
-                          features.strIngredient17}
-                      </li>
-                      <li>
-                        {features.strIngredient18 !== "" &&
-                          features.strIngredient18}
-                      </li>
-                      <li>
-                        {features.strIngredient19 !== "" &&
-                          features.strIngredient19}
-                      </li>
-                      <li>
-                        {features.strIngredient20 !== "" &&
-                          features.strIngredient20}
-                      </li>
+                      {features.strIngredient1?.length ? (
+                        <li>{features.strIngredient1}</li>
+                      ) : (
+                        ""
+                      )}
+                      {features.strIngredient2?.length ? (
+                        <li>{features.strIngredient2}</li>
+                      ) : (
+                        ""
+                      )}
+                      {features.strIngredient3?.length ? (
+                        <li>{features.strIngredient3}</li>
+                      ) : (
+                        ""
+                      )}
+                      {features.strIngredient4?.length ? (
+                        <li>{features.strIngredient4}</li>
+                      ) : (
+                        ""
+                      )}
+                      {features.strIngredient5?.length ? (
+                        <li>{features.strIngredient5}</li>
+                      ) : (
+                        ""
+                      )}
+                      {features.strIngredient6?.length ? (
+                        <li>{features.strIngredient6}</li>
+                      ) : (
+                        ""
+                      )}
+                      {features.strIngredient7?.length ? (
+                        <li>{features.strIngredient7}</li>
+                      ) : (
+                        ""
+                      )}
+                      {features.strIngredient8?.length ? (
+                        <li>{features.strIngredient8}</li>
+                      ) : (
+                        ""
+                      )}
+                      {features.strIngredient9?.length ? (
+                        <li>{features.strIngredient9}</li>
+                      ) : (
+                        ""
+                      )}
+                      {features.strIngredient10?.length ? (
+                        <li>{features.strIngredient10}</li>
+                      ) : (
+                        ""
+                      )}
+                      {features.strIngredient11?.length ? (
+                        <li>{features.strIngredient11}</li>
+                      ) : (
+                        ""
+                      )}
+                      {features.strIngredient12?.length ? (
+                        <li>{features.strIngredient12}</li>
+                      ) : (
+                        ""
+                      )}
+                      {features.strIngredient13?.length ? (
+                        <li>{features.strIngredient13}</li>
+                      ) : (
+                        ""
+                      )}
+                      {features.strIngredient14?.length ? (
+                        <li>{features.strIngredient14}</li>
+                      ) : (
+                        ""
+                      )}
+                      {features.strIngredient15?.length ? (
+                        <li>{features.strIngredient15}</li>
+                      ) : (
+                        ""
+                      )}
+                      {features.strIngredient16?.length ? (
+                        <li>{features.strIngredient16}</li>
+                      ) : (
+                        ""
+                      )}
+                      {features.strIngredient17?.length ? (
+                        <li>{features.strIngredient17}</li>
+                      ) : (
+                        ""
+                      )}
+                      {features.strIngredient18?.length ? (
+                        <li>{features.strIngredient18}</li>
+                      ) : (
+                        ""
+                      )}
+                      {features.strIngredient19?.length ? (
+                        <li>{features.strIngredient19}</li>
+                      ) : (
+                        ""
+                      )}
+                      {features.strIngredient20?.length ? (
+                        <li>{features.strIngredient20}</li>
+                      ) : (
+                        ""
+                      )}
                     </ul>
                   </div>
+
                   <div className="col-md-6">
                     <h5>Measurnments</h5>
                     <ul>
-                      <li>{features.strMeasure1}</li>
-                      <li>{features.strMeasure2}</li>
-                      <li>{features.strMeasure3}</li>
-                      <li>{features.strMeasure4}</li>
-                      <li>{features.strMeasure5}</li>
-                      <li>{features.strMeasure6}</li>
-                      <li>{features.strMeasure7}</li>
-                      <li>{features.strMeasure8}</li>
-                      <li>{features.strMeasure9}</li>
-                      <li>{features.strMeasure10}</li>
-                      <li>{features.strMeasure11}</li>
-                      <li>{features.strMeasure12}</li>
-                      <li>{features.strMeasure13}</li>
-                      <li>{features.strMeasure14}</li>
-                      <li>{features.strMeasure15}</li>
-                      <li>{features.strMeasure16}</li>
-                      <li>{features.strMeasure17}</li>
-                      <li>{features.strMeasure18}</li>
-                      <li>{features.strMeasure19}</li>
-                      {features.strMeasure20?.length && <li>{features.strMeasure20}</li>}
+                      {features.strMeasure1?.length ? (
+                        <li>{features.strMeasure1}</li>
+                      ) : (
+                        ""
+                      )}
+                      {features.strMeasure2?.length ? (
+                        <li>{features.strMeasure2}</li>
+                      ) : (
+                        ""
+                      )}
+                      {features.strMeasure3?.length ? (
+                        <li>{features.strMeasure3}</li>
+                      ) : (
+                        ""
+                      )}
+                      {features.strMeasure4?.length ? (
+                        <li>{features.strMeasure4}</li>
+                      ) : (
+                        ""
+                      )}
+                      {features.strMeasure5?.length ? (
+                        <li>{features.strMeasure5}</li>
+                      ) : (
+                        ""
+                      )}
+                      {features.strMeasure6?.length ? (
+                        <li>{features.strMeasure6}</li>
+                      ) : (
+                        ""
+                      )}
+                      {features.strMeasure7?.length ? (
+                        <li>{features.strMeasure7}</li>
+                      ) : (
+                        ""
+                      )}
+                      {features.strMeasure8?.length ? (
+                        <li>{features.strMeasure8}</li>
+                      ) : (
+                        ""
+                      )}
+                      {features.strMeasure9?.length ? (
+                        <li>{features.strMeasure9}</li>
+                      ) : (
+                        ""
+                      )}
+                      {features.strMeasure10?.length ? (
+                        <li>{features.strMeasure10}</li>
+                      ) : (
+                        ""
+                      )}
+                      {features.strMeasure11?.length ? (
+                        <li>{features.strMeasure11}</li>
+                      ) : (
+                        ""
+                      )}
+                      {features.strMeasure12?.length ? (
+                        <li>{features.strMeasure12}</li>
+                      ) : (
+                        ""
+                      )}
+                      {features.strMeasure13?.length ? (
+                        <li>{features.strMeasure13}</li>
+                      ) : (
+                        ""
+                      )}
+                      {features.strMeasure14?.length ? (
+                        <li>{features.strMeasure14}</li>
+                      ) : (
+                        ""
+                      )}
+                      {features.strMeasure15?.length ? (
+                        <li>{features.strMeasure15}</li>
+                      ) : (
+                        ""
+                      )}
+                      {features.strMeasure16?.length ? (
+                        <li>{features.strMeasure16}</li>
+                      ) : (
+                        ""
+                      )}
+                      {features.strMeasure17?.length ? (
+                        <li>{features.strMeasure17}</li>
+                      ) : (
+                        ""
+                      )}
+                      {features.strMeasure18?.length ? (
+                        <li>{features.strMeasure18}</li>
+                      ) : (
+                        ""
+                      )}
+                      {features.strMeasure19?.length ? (
+                        <li>{features.strMeasure19}</li>
+                      ) : (
+                        ""
+                      )}
+                      {features.strMeasure20?.length ? (
+                        <li>{features.strMeasure20}</li>
+                      ) : (
+                        ""
+                      )}
                     </ul>
                   </div>
                 </div>
