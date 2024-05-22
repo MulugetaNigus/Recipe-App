86635d825922288f882bba0ad0d5a3da156cde3c
MulugetaNigus <mullerhihi@gmail.com>
2024-05-22 21:53:42 +0300

    adding more logic, when the data fetch from the api

--- src/components/CommonRecipe.tsx
+++ src/components/CommonRecipe.tsx
@@ -17,47 +17,22 @@ function CommonRecipe() {
           <div
             className="col-md-3 fs-6 fw-bold p-0 rounded"
             style={{ color: "rgb(78,65,70)" }}
-          id="additionalSec"
-            
+            id="additionalSec"
           >
             <div className="card">
               <img
-                src="https://images.unsplash.com/photo-1546069901-ba9599a7e63c?w=500&auto=format&fit=crop&q=60&ixlib=rb-4.0.3&ixid=M3wxMjA3fDB8MHxzZWFyY2h8NHx8Zm9vZHxlbnwwfHwwfHx8MA%3D%3D"
+                src="https://upload.wikimedia.org/wikipedia/commons/thumb/f/fe/Carrot_cake_at_America_Graffiti_%28cropped%29.jpg/220px-Carrot_cake_at_America_Graffiti_%28cropped%29.jpg"
                 className="card-img-top"
                 alt="..."
               />
               <div className="card-body">
-                <h5 className="card-title">Card title</h5>
+                <h5 className="card-title">Carrot cake</h5>
                 <p className="card-text">
-                  Some quick example text to build on the card title and make up
-                  the bulk of the card's content.
+                  Carrot cake (also known as passion cake) is cake that contains
+                  carrots mixed into the batter.
                 </p>
-                <a href="#" className="btn btn-primary">
-                  Go somewhere
-                </a>
-              </div>
-            </div>
-          </div>
-          <div
-            className="col-md-3 fs-6 fw-bold p-0 rounded"
-            style={{ color: "rgb(78,65,70)" }}
-          id="additionalSec"
-            
-          >
-            <div className="card">
-              <img
-                src="https://images.unsplash.com/photo-1546069901-ba9599a7e63c?w=500&auto=format&fit=crop&q=60&ixlib=rb-4.0.3&ixid=M3wxMjA3fDB8MHxzZWFyY2h8NHx8Zm9vZHxlbnwwfHwwfHx8MA%3D%3D"
-                className="card-img-top"
-                alt="..."
-              />
-              <div className="card-body">
-                <h5 className="card-title">Card title</h5>
-                <p className="card-text">
-                  Some quick example text to build on the card title and make up
-                  the bulk of the card's content.
-                </p>
-                <a href="#" className="btn btn-primary">
-                  Go somewhere
+                <a href="findRecipe" className="btn btn-light">
+                  Search Another
                 </a>
               </div>
             </div>
@@ -69,18 +44,43 @@ function CommonRecipe() {
           >
             <div className="card">
               <img
-                src="https://images.unsplash.com/photo-1546069901-ba9599a7e63c?w=500&auto=format&fit=crop&q=60&ixlib=rb-4.0.3&ixid=M3wxMjA3fDB8MHxzZWFyY2h8NHx8Zm9vZHxlbnwwfHwwfHx8MA%3D%3D"
+                src="https://www.themealdb.com/images/media/meals/xqusqy1487348868.jpg"
                 className="card-img-top"
                 alt="..."
               />
               <div className="card-body">
-                <h5 className="card-title">Card title</h5>
+                <h5 className="card-title">Chicken nugget</h5>
                 <p className="card-text">
-                  Some quick example text to build on the card title and make up
-                  the bulk of the card's content.
+                  A chicken nugget is a food product consisting of a small piece
+                  of deboned chicken meat that is breaded or battered, then
+                  deep-fried or baked.
                 </p>
-                <a href="#" className="btn btn-primary">
-                  Go somewhere
+                <a href="findRecipe" className="btn btn-light">
+                  Search Another
+                </a>
+              </div>
+            </div>
+          </div>
+          <div
+            className="col-md-3 fs-6 fw-bold p-0 rounded"
+            style={{ color: "rgb(78,65,70)" }}
+            id="additionalSec"
+          >
+            <div className="card">
+              <img
+                src="https://upload.wikimedia.org/wikipedia/commons/thumb/4/43/Blueberry_pancakes_%283%29.jpg/220px-Blueberry_pancakes_%283%29.jpg"
+                className="card-img-top"
+                alt="..."
+              />
+              <div className="card-body">
+                <h5 className="card-title">Pancake</h5>
+                <p className="card-text">
+                  A pancake (or hotcake, griddlecake, or flapjack) is a flat
+                  cake, often thin and round, prepared from a starch-based
+                  batter that may contain eggs, milk and butter
+                </p>
+                <a href="findRecipe" className="btn btn-light">
+                  Search Another
                 </a>
               </div>
             </div>
@@ -89,25 +89,25 @@ function CommonRecipe() {
       </div>
       <div className="row m-2 mb-5 d-flex flex-row align-items-center justify-content-center gap-4">
         <div
-          className="col-md-3 fs-6 fw-bold p-0 rounded"
+          className="col-md-3 fs-6 fw-bold rounded"
           style={{ color: "rgb(78,65,70)" }}
           id="additionalSec"
-          
         >
           <div className="card">
             <img
-              src="https://images.unsplash.com/photo-1546069901-ba9599a7e63c?w=500&auto=format&fit=crop&q=60&ixlib=rb-4.0.3&ixid=M3wxMjA3fDB8MHxzZWFyY2h8NHx8Zm9vZHxlbnwwfHwwfHx8MA%3D%3D"
+              src="https://upload.wikimedia.org/wikipedia/commons/thumb/8/80/Lomo_Saltado_-_Lima%2C_Peru_Miraflores_%28Tiendecita_Blanca%29.jpg/250px-Lomo_Saltado_-_Lima%2C_Peru_Miraflores_%28Tiendecita_Blanca%29.jpg"
               className="card-img-top"
               alt="..."
             />
             <div className="card-body">
-              <h5 className="card-title">Card title</h5>
+              <h5 className="card-title">Lomo saltado</h5>
               <p className="card-text">
-                Some quick example text to build on the card title and make up
-                the bulk of the card's content.
+                Lomo saltado is a popular, traditional Peruvian dish, a stir fry
+                that typically combines marinated strips of sirloin (or other
+                beef steak) with onions, tomatoes...
               </p>
-              <a href="#" className="btn btn-primary">
-                Go somewhere
+              <a href="findRecipe" className="btn btn-light">
+                Search Another
               </a>
             </div>
           </div>
@@ -119,18 +119,17 @@ function CommonRecipe() {
         >
           <div className="card">
             <img
-              src="https://images.unsplash.com/photo-1546069901-ba9599a7e63c?w=500&auto=format&fit=crop&q=60&ixlib=rb-4.0.3&ixid=M3wxMjA3fDB8MHxzZWFyY2h8NHx8Zm9vZHxlbnwwfHwwfHx8MA%3D%3D"
+              src="https://www.themealdb.com/images/media/meals/4i5cnx1587672171.jpg"
               className="card-img-top"
               alt="..."
             />
             <div className="card-body">
-              <h5 className="card-title">Card title</h5>
+              <h5 className="card-title">Doughnut</h5>
               <p className="card-text">
-                Some quick example text to build on the card title and make up
-                the bulk of the card's content.
+              A doughnut or donut (/ˈdoʊnət/) is a type of pastry made from leavened fried dough.F
               </p>
-              <a href="#" className="btn btn-primary">
-                Go somewhere
+              <a href="findRecipe" className="btn btn-light mb-3">
+                Search Another
               </a>
             </div>
           </div>
@@ -142,18 +141,19 @@ function CommonRecipe() {
         >
           <div className="card">
             <img
-              src="https://images.unsplash.com/photo-1546069901-ba9599a7e63c?w=500&auto=format&fit=crop&q=60&ixlib=rb-4.0.3&ixid=M3wxMjA3fDB8MHxzZWFyY2h8NHx8Zm9vZHxlbnwwfHwwfHx8MA%3D%3D"
+              src="https://upload.wikimedia.org/wikipedia/commons/thumb/e/e8/Arepa_de_pabellon.jpg/220px-Arepa_de_pabellon.jpg"
               className="card-img-top"
               alt="..."
             />
             <div className="card-body">
               <h5 className="card-title">Card title</h5>
               <p className="card-text">
-                Some quick example text to build on the card title and make up
-                the bulk of the card's content.
+                Arepa (Spanish pronunciation: [aˈɾepa]) is a type of flatbread
+                made of ground maize dough stuffed with a filling, eaten in
+                northern parts of South America
               </p>
-              <a href="#" className="btn btn-primary">
-                Go somewhere
+              <a href="findRecipe" className="btn btn-light">
+                Search Another
               </a>
             </div>
           </div>
